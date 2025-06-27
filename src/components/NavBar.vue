bacde87a104cedc8c5b274ff2bda7d7b2efe61b0
Will Hines <bhines7@gmail.com>
2025-06-27 13:58:43 -0500

    Major navbar stylistic changes. Not perfect but significantly better.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,14 @@
 <!--Header component with links to other parts of the doc-->
 <template>
-  <nav class="navbar fixed-top navbar-expand navbar-dark bg-dark">
-    <div class="container-fluid justify-content-center">
+  <nav class="navbar fixed-top navbar-expand navbar-dark">
+    <div class="container flex justify-between items-center pt-2">
       <div class="navbar-header">
-        <a class="navbar-brand" href="#">William Hines</a>
+        <a class="navbar-brand font-weight-bold" href="#">William Hines
+          <span class="text-success">.</span>
+        </a>
       </div>
       <ul class="nav navbar-nav mb-2 mb-lg-0">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
         <li class="nav-item"><a href="#skills" class="nav-link">Skills</a></li>
         <li class="nav-item"><a href="#projects" class="nav-link">Projects</a></li>
         <li class="nav-item"><a href="#links" class="nav-link">Links</a></li>
@@ -23,8 +25,6 @@ export default {
 }
 </script>
 
-<style scoped>
-nav .navbar-nav li a{
-  color: white;
-  }
+<style>
+
 </style>
