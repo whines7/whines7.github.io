d332efe5c3fb0c296c92c9bc1515e5e225dc409c
Will Hines <bhines7@gmail.com>
2025-08-02 18:59:57 -0500

    Starting final page of site. Just need some proper images then can finish it all off.

--- src/components/projects/MyProjects.vue
+++ src/components/projects/MyProjects.vue
@@ -1,7 +1,28 @@
 <template>
-  <div id="projects" class="projects text-danger">
+  <div id="projects" class="projects text-success mt-5">
     <div class="container">
-      <h1>This is the projects section</h1>
+      <div class="row">
+        <h1>Projects</h1>
+      </div>
+      <div class="row">
+        <h5>(And other small things I've worked on.)</h5>
+      </div>
+      <div class="row">
+        <div class="col-sm-6">
+          <!-- This will be a picture of the homepage once I Have a decent picture to use of myself.-->
+          <h3>Personal Website</h3>
+          <p>
+            This website is my personal portfolio. It was built using Vue.js and Bootstrap.
+            It is hosted on GitHub Pages and the source code can be found on my GitHub.
+          </p>
+        </div>
+        <div class="col-sm-6">
+          <p>Home Server</p>
+        </div>
+      </div>
+      <div class="row">
+        <h3>Slider Helper</h3>
+      </div>
     </div>
   </div>
 </template>
